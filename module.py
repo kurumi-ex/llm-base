3ab02c0d9e3e93da2c6498aebb4b61f13eeb37d5
kurumi <1183580621@qq.com>
2025-09-26 22:29:31 +0800

    impl: moe

--- module.py
+++ module.py
@@ -62,37 +62,98 @@ class Expert(nn.Module):
 
 
 class MoE(nn.Module):
-    def __init__(self, in_features, hidden_dims, expert_nums, top_k, expert_capacity):
+    def __init__(self, in_features: int, hidden_dims: int, expert_nums: int, top_k: int, expert_capacity):
         super().__init__()
         self.expert_nums = expert_nums
         self.top_k = top_k
         self.capacity = expert_capacity
+        self.out_features = in_features
         self.experts = nn.ModuleList(
             [Expert(in_features, in_features, hidden_dims) for _ in range(expert_nums)]
         )
         self.ffn = nn.Linear(in_features, expert_nums)
 
     def forward(self, x):
+        device = x.device
+        batch_size = x.shape[0]
         y = self.ffn(x)
         scores = F.softmax(y, dim=-1)
-        topk_probs, topk_indices = torch.topk(scores, dim=-1, k = self.top_k)
+        top_k_probs, top_k_indices = torch.topk(scores, dim=-1, k=self.top_k)
+        flat_probs = top_k_probs.view(-1)
+        flat_indices = top_k_indices.view(-1)
 
-        for expert in self.experts:
-            # shi1
-            cur_y = expert(x)
+        # 生成列的索引采样
+        sampled_indices = torch.arange(batch_size).reshape(batch_size, 1)
 
-        weights = []
-        expert_out = []
-        outputs = weights * expert_out
+        # 选择最后一维度扩容到 topk
+        sampled_indices = sampled_indices.expand(-1, self.top_k).flatten()
+
+        outputs = torch.zeros(batch_size, self.out_features, device=device)
+
+        if self.training:
+            importance = scores.sum(dim=0)
+            aux_loss = torch.var(importance) / torch.mean(importance)
+
+        else:
+            aux_loss = 0
+
+        for idx in range(self.expert_nums):
+            expert_mask = idx == flat_indices
+            exp_samples = sampled_indices[expert_mask]
+            exp_weights = flat_probs[expert_mask]
+
+            if len(exp_samples) == 0:
+                # 未选中当前专家
+                continue
+
+            # exp_samples是选择的索引列表 将目标的x读入进来
+            expert_input = x[exp_samples]
+
+            # 在选择的维度增加一个容量为一的维度
+            expert_output = self.experts[idx](expert_input) * exp_weights.unsqueeze(-1)
+
+            # 在选择的维度和索引上进行相加
+            outputs.index_add_(0, exp_samples, expert_output)
+
+        return outputs, aux_loss
 
 
 if __name__ == "__main__":
     # expert = Expert(in_features=100, out_features=100, hidden_dims=64)
     # myinput = torch.randn(3, 100)
     # print(expert(myinput))
+    # x = torch.randn(6, 3)
+    # x = F.softmax(x, dim=-1)
+    # print(x)
+    # _, _x = torch.topk(x, dim=-1, k=2)
+    # print(_x)
+    # print(_)
+    #
+    # print(_.view(-1))
+    #
+    # print(torch.arange(5).reshape(5, 1))
+    # tmp = torch.arange(5)[:, None]
+    # print(tmp.expand(-1, 2))
+    #
+    # print(x * torch.tensor([1, 2, 3, 4, 5, 6]).unsqueeze(-1))
+
+    # batch_size = 128
+    #
+    # x = torch.randn(batch_size, 12, 64, requires_grad=True)
+    #
+    # model = MoE(
+    #     in_features=64,
+    #     hidden_dims=256,
+    #     expert_nums=10,
+    #     top_k=2,
+    #     expert_capacity=10,
+    # )
+    #
+    # model.eval()
+    #
+    # y, _ = model(x.reshape(-1, 64))
+    # print(y.shape)
+
     x = torch.randn(2, 3)
-    x = F.softmax(x, dim=-1)
-    print(x)
-    _, _x = torch.topk(x, dim=-1, k=2)
-    print(_x)
-    print(_)
+    print(x.sum(dim=0))
+    print(torch.mean(x, dim=0))
